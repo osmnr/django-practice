01adb9e6fdc1296cc0fa9695b6ec275656cc07ab
osmnr <osmanrecepoglu00@gmail.com>
2024-08-26 17:54:31 +0300

    contactUs app is updated

--- contactUs/urls.py
+++ contactUs/urls.py
@@ -5,4 +5,6 @@ app_name = 'contactUs'
 
 urlpatterns = [
    path('', views.contactUs, name='contactUs'),
+   path('list/', views.contactList, name='contactList'),
+   path('detail/<int:id>/', views.contactUsDetail, name='contactUsDetail'),
 ]
--- contactUs/views.py
+++ contactUs/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import contactMessage
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 def contactUs(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
+        if request.user:
+            name = request.user.username
+            if request.user.email:
+                email = request.user.email
+            else:
+                email = request.POST.get('email')
+                ## saving the email to user table
+                a = User.objects.get(pk=request.user.id)
+                a.email =email
+                a.save()
+        else:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
         subject = request.POST.get('subject')
         message = request.POST.get('message')
         if len(subject) <= 180 and len(subject) >= 5 and len(name) <= 200 and len(name) >= 3:
@@ -23,3 +37,28 @@ def contactUs(request):
 
 
 
+def contactList(request):
+    if request.user.is_staff:
+        contactMessageList = contactMessage.objects.all()
+        paginator = Paginator(contactMessageList, 5)
+        pageNum = request.GET.get('page')
+        filteredMessageList = paginator.get_page(pageNum)   
+        data = {
+            'filteredMessageList':filteredMessageList,
+        }
+    else:
+        return redirect('home:home')
+    return render(request,"contactUs/contactList.html",data)
+
+def contactUsDetail(request,id):
+    if request.user.is_staff:
+        try:
+            contactUsMessage = contactMessage.objects.get(pk=id)
+            data = {
+                'contactUsMessage':contactUsMessage
+            }
+        except contactMessage.DoesNotExist:
+            return redirect('contactUs:contactList')
+    else:
+        return redirect('home:home')
+    return render(request,"contactUs/contactDetail.html",data)
